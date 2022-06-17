e4ba8452be8f70eb7e4d3754455a088dfd96a7f6
eram-iw <eram.perween@iwebcode.dev>
2022-06-17 10:22:28 +0530

    created ui for login and sign up, added mongodb to backend and connected backend with frontentend as well

---
+++ backend/data/data.js
@@ -0,0 +1,49 @@
+const chats=[
+    {
+        isGroupChat:false,
+        users:[
+            {
+                name:"Test 1",
+                email:"test1@gmail.com"
+            },
+            {
+                name:"Test 2",
+                email:"test2@gmail.com"
+            },
+        ],
+        _id:"chatid1",
+        chatName:"Test 1"
+    },
+    {
+        isGroupChat:false,
+        users:[
+            {
+                name:"Test 3",
+                email:"test3@gmail.com"
+            },
+            {
+                name:"Test 2",
+                email:"test2@gmail.com"
+            },
+        ],
+        _id:"chatid2",
+        chatName:"Test 3"
+    },
+    {
+        isGroupChat:false,
+        users:[
+            {
+                name:"Test 1",
+                email:"test1@gmail.com"
+            },
+            {
+                name:"Test 3",
+                email:"test3@gmail.com"
+            },
+        ],
+        _id:"chatid3",
+        chatName:"Test 1"
+    }
+]
+
+module.exports={chats}
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const { chats } = require('./data/data')
+const connectDB=require('./config/db.js')
+
+const app = express()
+
+dotenv.config();
+connectDB()
+
+app.get('/', (req, res) => {
+    res.send("API is working")
+})
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats)
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running on PORT ${PORT}`))
