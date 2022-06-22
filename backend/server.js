66f713886c9c74afceef028d00b726391b869450
eram-iw <eram.perween@iwebcode.dev>
2022-06-22 17:06:17 +0530

    implemented real time msg sending with socket io and added animation when a user is typing

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const connectDB=require('./config/db.js')
-const userRoutes=require('./routes/userRoutes')
-const chatRoutes=require('./routes/chatRoutes')
-const messageRoutes=require('./routes/messageRoutes')
+const connectDB = require('./config/db.js')
+const userRoutes = require('./routes/userRoutes')
+const chatRoutes = require('./routes/chatRoutes')
+const messageRoutes = require('./routes/messageRoutes')
 const { notFound, errorhandler } = require('./middleware/errorMiddleWare')
 
 const app = express()
@@ -17,13 +17,51 @@ app.get('/', (req, res) => {
     res.send("API is working")
 })
 
-app.use('/api/user',userRoutes)
-app.use('/api/chat',chatRoutes)
-app.use('/api/message',messageRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/chat', chatRoutes)
+app.use('/api/message', messageRoutes)
 
 app.use(notFound)
 app.use(errorhandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running on PORT ${PORT}`))
+const server = app.listen(PORT, console.log(`Server running on PORT ${PORT}`))
+
+const io = require('socket.io')(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: "http://localhost:3000"
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log('connected to socket.io');
+
+    socket.on('setup', (userData) => {
+        socket.join(userData._id);
+        socket.emit('connected')
+    })
+    socket.on('join chat', (room) => {
+        socket.join(room);
+        console.log('User Joined Room: ' + room);
+    })
+    socket.on('new message', (newMessageRecieved) => {
+        var chat=newMessageRecieved.chat;
+
+        if(!chat.users) return console.log('chat.users not defined');
+        chat.users.forEach(user=>{
+            if(user._id==newMessageRecieved.sender._id) return;
+
+            socket.in(user._id).emit("message recieved",newMessageRecieved)
+        })
+    })
+    socket.on('typing', (room) => socket.in(room).emit("typing"))
+    socket.on('stop typing', (room) => socket.in(room).emit("stop typing"))
+
+    socket.off('setup', () => {
+        console.log("USER DISCONNECTED");
+        socket.leave(userData._id);
+    })
+})
+
