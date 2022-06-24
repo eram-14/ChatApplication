9633e034ad1b955f459c485f8ca6c6a6ae02a69e
eram-iw <eram.perween@iwebcode.dev>
2022-06-24 11:08:10 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,10 @@ const path = require('path')
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === 'production') {
 
-    const root = require('path').join(__dirname1, 'frontend', 'build', 'index.html')
+    const root = require('path').join('frontend', 'build')
     app.use(express.static(root));
     app.get("*", (req, res) => {
-        res.sendFile(root);
+        res.sendFile('index.html', { root });
     })
 
 } else {
